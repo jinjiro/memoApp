90724739fd64187597ebf007fe0a45eaf6d43415
t_onzoshi <onzoshi@toyonaiacbookea.lan>
2019-05-21 22:46:52 +0900

    add icon font

--- memo/src/elements/CircleButton.js
+++ memo/src/elements/CircleButton.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Font } from 'expo';
+import { createIconSet } from '@expo/vector-icons';
 import fontAwesome from '../../assets/fonts/fa-solid-900.ttf';
+
+const CustomIcon = createIconSet({
+  pencil: '\uf303',
+  plus: '\uf067',
+}, 'FontAwesome');
 
 class CircleButton extends React.Component {
   state = {
@@ -17,7 +23,7 @@ class CircleButton extends React.Component {
   }
 
   render() {
-    const { style, color } = this.props;
+    const { name, style, color } = this.props;
 
     let bgColor = '#E31676';
     let textColor = '#fff';
@@ -31,9 +37,7 @@ class CircleButton extends React.Component {
       <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
         {
           this.state.fontLoaded ? (
-            <Text style={[styles.circleButtonTitle, { color: textColor }]}>
-              {this.props.children}
-            </Text>
+            <CustomIcon name={name} style={[styles.circleButtonTitle, { color: textColor }]} />
           ) : null
         }
       </View>
